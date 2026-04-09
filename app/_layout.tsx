1b0437b053410441687f39b8cbbd5620fc90c4eb
bekidink <bereketdinku876@gmail.com>
2026-04-09 07:54:22 +0300

    feat: integrate Tanstack query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,18 +2,20 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-
+import "@/global.css"
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '@/lib/queryClient';
 
 
 
 export default function RootLayout() {
   
   return (
-   
+   <QueryClientProvider client={queryClient}>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="detail/[name]"  />
       </Stack>
-      
+      </QueryClientProvider>
   );
 }
